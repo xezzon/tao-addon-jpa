b3f0b7bf39761f40957ea0465018e6a2c5edf43b
xezzon <xezzon@outlook.com>
2023-02-01 08:36:49 +0800

    JPA 局部更新语句

--- src/main/java/indi/xezzon/tao/jpa/JpaUtil.java
+++ src/main/java/indi/xezzon/tao/jpa/JpaUtil.java
@@ -1,11 +1,25 @@
 package indi.xezzon.tao.jpa;
 
+import cn.hutool.core.util.ReflectUtil;
+import com.querydsl.core.types.Path;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.impl.JPAUpdateClause;
 import indi.xezzon.tao.retrieval.CommonQuery;
+import java.lang.reflect.Field;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.persistence.Column;
+import javax.persistence.Version;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
@@ -16,6 +30,43 @@ import org.springframework.data.domain.Sort.Direction;
 public class JpaUtil {
 
   public static final BooleanExpression TRUE_EXPRESSION = Expressions.ONE.eq(1);
+
+  /**
+   * 局部更新语句
+   * @param obj 更新的信息
+   * @param queryFactory update语句
+   * @param dataObj DO对象
+   * @return 拼接后的update语句
+   */
+  @SuppressWarnings({"unchecked", "rawtypes"})
+  public static <T> JPAUpdateClause getUpdateClause(
+      T obj,
+      JPAQueryFactory queryFactory,
+      EntityPathBase<T> dataObj
+  ) {
+    JPAUpdateClause clause = queryFactory.update(dataObj);
+    LocalDateTime current = LocalDateTime.now();
+    Set<Field> fields = Arrays.stream(obj.getClass().getDeclaredFields())
+        .filter(field -> Objects.nonNull(field.getAnnotation(Column.class)))
+        .filter(field -> field.getAnnotation(Column.class).updatable())
+        .collect(Collectors.toSet());
+    for (Field field : fields) {
+      Object column = ReflectUtil.getFieldValue(dataObj, field.getName());
+      Path path = (Path) column;
+      Object value = ReflectUtil.getFieldValue(obj, field.getName());
+      if (field.isAnnotationPresent(LastModifiedDate.class)) {
+        clause.set(path, current);
+      }
+      if (value != null) {
+        clause.set(path, value);
+      }
+      SimpleExpression expression = (SimpleExpression) column;
+      if (field.isAnnotationPresent(Version.class)) {
+        clause.where(expression.eq(value));
+      }
+    }
+    return clause;
+  }
 
   public static <T extends EntityPathBase<RT>, RT> BooleanExpression getQueryClause(
       CommonQuery commonQuery,
