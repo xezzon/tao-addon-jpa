5557939577445b46aec15bfe6d81cf2506abe2f2
xezzon <xezzon@outlook.com>
2023-04-17 13:44:42 +0800

    [test] 正常查询
    含 AND、OR 的组合筛选，排序，分页。

--- src/test/java/io/github/xezzon/tao/jpa/JpaWrapperTest.java
+++ src/test/java/io/github/xezzon/tao/jpa/JpaWrapperTest.java
@@ -6,13 +6,17 @@ import io.github.xezzon.tao.retrieval.CommonQuery;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,7 +52,27 @@ class JpaWrapperTest {
    */
   @Test
   void query() {
+    String condition = "(name LLIKE 'J' OR (age GT 18)) AND (gender IN 'MALE' OR deleteTime NULL true)";
+    List<User> excepts = UserDataset.getDataset().parallelStream()
+        .filter(user -> user.getName().startsWith("J") || user.getAge() > 18)
+        .filter(user -> Objects.equals(GenderEnum.MALE, user.getGender())
+            || user.getDeleteTime() == null)
+        .sorted(Comparator.comparing(User::getCredit))
+        .toList();
+    int pageSize = 15;
+    int pageNum = 1;
 
+    CommonQuery commonQuery = new CommonQuery();
+    commonQuery.setFilter(condition);
+    commonQuery.setSort(Collections.singletonList("credit:ASC"));
+    commonQuery.setPageSize(pageSize);
+    commonQuery.setPageNum(pageNum);
+    Page<User> users = userDAO.query(commonQuery);
+    Assertions.assertEquals(excepts.size(), users.getTotalElements());
+    Assertions.assertIterableEquals(
+        excepts.parallelStream().limit(pageSize).toList(),
+        users.getContent()
+    );
   }
 
   /**
@@ -112,6 +136,7 @@ class JpaWrapperTest {
 
 @Getter
 @Setter
+@ToString
 @Entity
 @Table(name = "t_user")
 class User {
@@ -123,12 +148,29 @@ class User {
   private String name;
   @Column
   private Integer age;
-  @Column
+  @Column(scale = 9)
   private BigDecimal credit;
   @Column
   private GenderEnum gender;
   @Column
   private LocalDateTime deleteTime;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    User user = (User) o;
+    return Objects.equals(id, user.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
 
 enum GenderEnum {
@@ -148,7 +190,7 @@ class UserDataset extends AbstractDataset<User> {
       user.setId(IdUtil.getSnowflakeNextIdStr());
       user.setName(RandomUtil.randomString(6));
       user.setAge(RandomUtil.randomInt(6, 60));
-      user.setCredit(RandomUtil.randomBigDecimal());
+      user.setCredit(RandomUtil.randomBigDecimal(new BigDecimal("100.000")));
       user.setGender(RandomUtil.randomEle(GenderEnum.values()));
       user.setDeleteTime(RandomUtil.randomBoolean() ?
           LocalDateTime.of(
