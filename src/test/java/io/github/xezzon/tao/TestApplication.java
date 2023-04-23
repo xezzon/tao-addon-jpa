38bf3bfbd9db783995b0e31e3293610c388831e4
xezzon <xezzon@outlook.com>
2023-04-23 13:40:39 +0800

    [test] 测试逻辑删除

--- src/test/java/io/github/xezzon/tao/TestApplication.java
+++ src/test/java/io/github/xezzon/tao/TestApplication.java
@@ -2,11 +2,13 @@ package io.github.xezzon.tao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author xezzon
  */
 @SpringBootApplication
+@EnableJpaAuditing
 public class TestApplication {
 
   public static void main(String[] args) {
